1db29184c69976d90e2997143002f15ba5255f4a
JoseMorocho <jmorocho@glajumedia.com>
2025-09-02 14:55:47 -0500

    feat: add cors middleware and update dependencies in package.json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { dpiRouter } from "./router/dpi.router.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/dpi", dpiRouter);
